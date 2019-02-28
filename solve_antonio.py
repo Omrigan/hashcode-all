d89423ccccee5282ebe6ec9de011e76f7aecbc37
aboffa <a.boffa95@gmail.com>
2019-02-28 22:33:33 +0200

    last brute force solution

--- solve_antonio.py
+++ solve_antonio.py
@@ -6,7 +6,9 @@ def computescore(x,y):
     b = y[1]
     return min(len(a&b), len(a-b), len(b-a))
 
-def solve_antonio(p : Problem, input ):
+iteration = 1
+
+def solve_antonio_sampling(p : Problem, input ):
     s = Solution(p)
     myset = set(input)
     current = myset.pop()
@@ -14,12 +16,9 @@ def solve_antonio(p : Problem, input ):
     while (len(myset)>0):
         tmp_max = -1
         selected_item = None
-        #for i, val in enumerate(myset):
-        #   if(i > 250):
-        #        break
-        for val in random.sample(myset, 200):
+        testset = random.sample(myset,min(iteration,len(myset)))
+        for val in testset: 
             tmp = computescore(current,val)
-            #print("comparing",current,val,tmp)
             if (tmp >= tmp_max):
                 tmp_max = tmp
                 selected_item = val
@@ -28,8 +27,31 @@ def solve_antonio(p : Problem, input ):
         myset.remove(selected_item)
     return s
 
+def solve_antonio_sequential(p : Problem, input ):
+    s = Solution(p)
+    myset = set(input)
+    current = myset.pop()
+    s.add_any(current[0])
+    while (len(myset)>0):
+        tmp_max = -1
+        selected_item = None
+        testset = random.sample(myset,min(iteration,len(myset)))
+        for i, val in enumerate(myset):
+            if(i > 50):
+                break
+            tmp = computescore(current,val)
+            if (tmp >= tmp_max):
+                tmp_max = tmp
+                selected_item = val
+        current = selected_item
+        s.add_any(selected_item[0])
+        myset.remove(selected_item)
+    return s
+
+'''
 if __name__ == "__main__":
     p = Problem('input2019/a_example.txt')
     samplelist = [(None,frozenset({"a","b"})),(None,frozenset({"a","e","b"})),(None,frozenset({"f","g"})),(None,frozenset({"f","g","h"}))]
     #s = solve_antonio(p, p.result_images )
     s = solve_antonio(p, samplelist )
+'''
