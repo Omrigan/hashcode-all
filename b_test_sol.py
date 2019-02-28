91630904a651bfe71ed705027b9a0f9774919d46
Omrigan <omrigann@gmail.com>
2019-02-28 21:45:51 +0200

    Andrew fixes

--- b_test_sol.py
+++ b_test_sol.py
@@ -4,21 +4,26 @@ import tqdm
 
 N = 80000
 
-def dfs(v, solution, used, g):
-    used[v] = True
-    solution.add_horizontal(v)
-    for u in g[v]:
-        if used[u] == False:
-            dfs(u, solution, used, g)
+
+
+
 
 def b_test_solve(p):
-    g = [[] for i in range(N)] 
-    used = [0] * N
+    g = [[] for i in range(N)]
+    used = [False for i in range(N)]
 
-    sys.setrecursionlimit(N)
+    sys.setrecursionlimit(10 * N)
 
     tags_ids = dict()
     solution = Solution(p)
+
+    def dfs(v):
+        used[v] = True
+        solution.add_horizontal(v)
+        for u in g[v]:
+            if used[u] == False:
+                # pass
+                dfs(u)
 
     for pic_id in tqdm.tqdm(range(p.num_pics)):
         for tag in p.tags[pic_id]:
@@ -26,13 +31,13 @@ def b_test_solve(p):
                 tags_ids[tag] = []
             tags_ids[tag].append(pic_id)
     for tag, tag_id_list in tags_ids.items():
-        assert(len(tag_id_list) <= 2)
+        assert (len(tag_id_list) <= 2)
         if len(tag_id_list) == 2:
             g[tag_id_list[0]].append(tag_id_list[1])
             g[tag_id_list[1]].append(tag_id_list[0])
 
     for i in range(N):
         if used[i] == False:
-            dfs(i, solution, used, g)
+            dfs(i)
 
     return solution
