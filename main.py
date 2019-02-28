ac36f0a25eb48eca6d744921845fa2379666acf9
NedoL98 <a.nedol@yandex.ru>
2019-02-28 22:37:22 +0300

    B solution

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from read_write import Problem, Solution
-
+import b_test_sol
 
 if __name__ == "__main__":
-    p = Problem('input2019/a_example.txt')
-    s = Solution(p);
-    s.add_horizontal(0)
-    s.add_vertical(1,2)
-    s.add_horizontal(3)
-    s.write('output/a_example_out.txt')
+    p = Problem('input2019/b_lovely_landscapes.txt')
+    # s = Solution(p);
+    # s.add_horizontal(0)
+    # s.add_vertical(1,2)
+    # s.add_horizontal(3)
+    s = b_test_sol.b_test_solve(p)
+    s.write('output/b_lovely_landscapes_out.txt')
     print("done")
